cd73218c4a0fb07ef2d715d5a8f408a4388a1a65
ramyanaga <ramyanaga.99@gmail.com>
2015-08-18 09:21:29 -0700

    APCS, Hello World
    My first CS program in Java.

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,14 @@
+
+/**
+ * This will do a simple println of Hello World. Our first java code. 
+ * 
+ * @author Ramya Nagarajan 
+ * @version (a version number or a date)
+ */
+public class HelloWorld {
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World! From Ramya Nagarajan"); 
+        //Display the string
+    }
+}
