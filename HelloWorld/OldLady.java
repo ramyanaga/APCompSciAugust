e967215a3167eea9f533bf16529a778bfdef06e8
ramyanaga <rrramya@Ramyas-MBP.att.net>
2015-08-26 07:38:28 -0700

    Old Lady Rhyme Code
    Prints the lyrics to the rhyme of “there was an old lady who…”

---
+++ HelloWorld/OldLady.java
@@ -0,0 +1,71 @@
+
+/**
+ * Write a description of class OldLady here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class OldLady{
+    public static void spider() {
+        System.out.println("She swallowed the spider to \n catch the fly"); 
+    }
+    public static void EndThreeL() {
+        System.out.println("I don't know why she \n swallowed the fly \n perhaps she'll die"); 
+        System.out.println(""); 
+    } 
+    public static void bird() {
+        System.out.println("She swallowed the bird \n to catch the spider"); 
+    }
+    public static void cat() {
+        System.out.println("She swallowed the cat to catch the bird"); 
+    }
+    public static void dog() {
+        System.out.println("She swallowed the dog to catch the cat"); 
+    }
+        public static void main(String[] args) {
+        System.out.println("There was an old lady who swallowed a fly"); 
+        System.out.println("I don't know why she swallowed the fly"); 
+        System.out.println("Perhaps she'll die"); 
+        System.out.println("There was an old lady who swallowed a spider"); 
+        System.out.println("that wriggled and jiggled \n and ticked inside here."); 
+        spider(); 
+        EndThreeL();
+        
+        System.out.println("There was an old lady who \n swallowed a bird \n How absurd! To swallow a bird"); 
+        bird(); 
+        spider(); 
+        EndThreeL(); 
+        
+        System.out.println("There was an old lady who \n swallowed a cat \n Imagine that! To swallow a cat."); 
+        cat(); 
+        bird(); 
+        spider(); 
+        EndThreeL(); 
+        
+        System.out.println("There was an old lady who \n swallowed a dog.\n What a hog! To swallow a dog."); 
+        dog(); 
+        cat(); 
+        bird(); 
+        spider(); 
+        EndThreeL(); 
+        
+        System.out.println("There was an old lady who \n swallowed a goat. Just opened her throat! To \n swallow a goat"); 
+        System.out.println("She swallowed the goat to \n catch the dog."); 
+        dog(); 
+        cat(); 
+        bird(); 
+        spider(); 
+        EndThreeL(); 
+        
+        System.out.println("There was an old lady who \n swallowed a cow. I don't know how she \n swallowed a cow!"); 
+        System.out.println("She swallowed the cow to \n catch the goat."); 
+        System.out.println("She swallowed the goat to \n catch the dog"); 
+        dog(); 
+        cat(); 
+        bird(); 
+        spider(); 
+        EndThreeL(); 
+        
+        System.out.println("There was an old lady who \n swallowed a horse. \n She died of course!"); 
+    }
+}
