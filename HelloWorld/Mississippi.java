0bdf1d9bf0e197526f50c3f3c14e240cc69792e3
ramyanaga <rrramya@Ramyas-MBP.att.net>
2015-08-25 20:52:10 -0700

    Mississippi Code
    Prints Mississippi Vertically
    Homework due Aug. 27

---
+++ HelloWorld/Mississippi.java
@@ -0,0 +1,64 @@
+
+/**
+ * Write a description of class Mississippi here.
+ * prints Mississippi vertically
+ * 
+ * @author Ramya Nagarajan
+ * @version 08/25/2015
+ */
+public class Mississippi {
+    public static void DrawM() {
+        System.out.println("M     M"); 
+        System.out.println("MM   MM"); 
+        System.out.println("M M M M");
+        System.out.println("M  M  M"); 
+        System.out.println("M     M"); 
+        System.out.println("M     M"); 
+        System.out.println("M     M"); 
+    }
+    public static void DrawI() {
+        System.out.println("IIIII"); 
+        System.out.println("  I  "); 
+        System.out.println("  I"); 
+        System.out.println("  I"); 
+        System.out.println("  I"); 
+        System.out.println("  I"); 
+        System.out.println("IIIII");
+    }
+    public static void DrawS() {
+        System.out.println(" SSSSS"); 
+        System.out.println("S     S"); 
+        System.out.println("S"); 
+        System.out.println(" SSSSS"); 
+        System.out.println("      S"); 
+        System.out.println("S     S"); 
+        System.out.println(" SSSSS "); 
+    }
+    public static void DrawP() {
+        System.out.println("PPPPPP"); 
+        System.out.println("P     P"); 
+        System.out.println("P     P"); 
+        System.out.println("PPPPPP"); 
+        System.out.println("P"); 
+        System.out.println("P");
+        System.out.println("P");
+    }    
+        
+        
+    public static void main(String[] args) {
+        DrawM(); 
+        DrawI(); 
+        DrawS(); 
+        DrawS(); 
+        DrawI(); 
+        DrawS(); 
+        DrawS(); 
+        DrawI(); 
+        DrawP(); 
+        DrawP(); 
+        DrawI(); 
+       
+    }
+}
+    
+        
